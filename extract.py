63e84146f025eab47de97f8a12a360fd1a9e68d6
Anders Green <anders.green@proceranetworks.com>
2016-04-10 22:15:43 +0200

    all functional code added
    a lot of debug code still in

--- extract.py
+++ extract.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import optparse
+
 import MySQLdb as mdb
 import sys
 reload(sys)
 sys.setdefaultencoding('iso-8859-15')
 import os.path
 import re
+re_latin1 = re.compile(r"[åäÅÄ]", re.IGNORECASE)
+re_latin2 = re.compile(r"[öÖ]", re.IGNORECASE)
 re_illigal = re.compile(r"[\[\]\\\/\?%:|\"'><#\s]", re.IGNORECASE)
 re_markup = re.compile(r"\[.*?\]")
 
@@ -15,7 +19,103 @@ h = HTMLParser.HTMLParser()
 
 from config import mysql
 
-dryrun = False
+from PIL import Image
+
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+
+
+# cli options
+def parse_options():
+    parser = optparse.OptionParser()
+    parser.add_option(
+        '-s',
+        '--source-path',
+        type='string',
+        dest='source_path',
+        help="Path to the full size images from Gallery 2 (probably g2data/albums)",
+        default=''
+    )
+
+    parser.add_option(
+        '-d',
+        '--debug',
+        action='store_true',
+        dest='debug',
+        help='Debug mode',
+        default=False
+    )
+
+    parser.add_option(
+        '--dry-run',
+        action='store_true',
+        dest='dry_run',
+        default=False
+    )
+
+    parser.add_option(
+        '--force-thumbs',
+        action='store_true',
+        dest='force_thumbs',
+        default=False
+    )
+
+    parser.add_option(
+        '--no-thumbs',
+        action='store_true',
+        dest='no_thumbs',
+        default=False
+    )
+
+    return vars(parser.parse_args()[0])
+
+im = None
+thumb = None
+
+
+def flat( *nums ):
+    'Build a tuple of ints from float or integer arguments. Useful because PIL crop and resize require integer points.'
+
+    return tuple( int(round(n)) for n in nums )
+
+class Size(object):
+    def __init__(self, pair):
+        self.width = float(pair[0])
+        self.height = float(pair[1])
+
+    @property
+    def aspect_ratio(self):
+        return self.width / self.height
+
+    @property
+    def size(self):
+        return flat(self.width, self.height)
+
+def cropped_thumbnail(img, size):
+    '''
+    Builds a thumbnail by cropping out a maximal region from the center of the original with
+    the same aspect ratio as the target size, and then resizing. The result is a thumbnail which is
+    always EXACTLY the requested size and with no aspect ratio distortion (although two edges, either
+    top/bottom or left/right depending whether the image is too tall or too wide, may be trimmed off.)
+    '''
+
+    original = Size(img.size)
+    target = Size(size)
+
+    if target.aspect_ratio > original.aspect_ratio:
+        # image is too tall: take some off the top and bottom
+        scale_factor = target.width / original.width
+        crop_size = Size( (original.width, target.height / scale_factor) )
+        top_cut_line = (original.height - crop_size.height) / 2
+        img = img.crop( flat(0, top_cut_line, crop_size.width, top_cut_line + crop_size.height) )
+    elif target.aspect_ratio < original.aspect_ratio:
+        # image is too wide: take some off the sides
+        scale_factor = target.height / original.height
+        crop_size = Size( (target.width/scale_factor, original.height) )
+        side_cut_line = (original.width - crop_size.width) / 2
+        img = img.crop( flat(side_cut_line, 0,  side_cut_line + crop_size.width, crop_size.height) )
+
+    return img.resize(target.size, Image.ANTIALIAS)
+
 
 SQLgetChildren = """SELECT
     ce.{0}id as id,    -- main id
@@ -60,20 +160,31 @@ all_files = []
 
 def decode(text):
     if text is None:
-        return None
+        return ""
     return text.encode("iso-8859-15","ignore")
 
 
-def generate_html(fname, grandchildren):
+def generate_html(fname, grandchildren, itemtype):
     html = "<html><body>"
     for grandchild in grandchildren:
-        html += "<a href='./{0}'>{1}</a><br/>".format(grandchild[0], grandchild[1])
+        pathcomponent = grandchild[0]
+        title = grandchild[1]
+        if itemtype == 'GalleryAlbumItem':
+            thumbcomponent = 't__album.jpg'
+        else:
+            thumbcomponent = pathcomponent
+
+        html += "<div class='object'><a href='./{0}'><img src='./t__{1}' width='150' height='150'/><div class='title'>{2}</div></a></div>".format(pathcomponent, thumbcomponent, title)
     html += "</body></html>"
 
     with open(fname, 'w') as f:
         f.write(html)
 
 def get_children(id, fspath, uipath, depth):
+    sys.stdout.write(">")
+    global im
+    global thumb
+
     cur.execute(SQLgetChildren, (id,));
     rows = cur.fetchall()
 
@@ -84,51 +195,120 @@ def get_children(id, fspath, uipath, depth):
         children = row[2]
 
         pathcomponent = decode(row[5])
-        title =re_markup.sub("", decode(row[3] or pathcomponent or "NoTitle"))
-        uipathcomponent = re_illigal.sub("_", h.unescape(title)).replace("__", "_").replace("_-_", "-").lower()
+        title = re_markup.sub("", decode(row[3])).replace('\00','')
+        if not title:
+            title = pathcomponent
+        if not title:
+            title = "notitle"
 
+        a=title
+        uipathcomponent = (title or pathcomponent).lower()
+        b = uipathcomponent
+        # replace latin1 stuff fast
+        uipathcomponent = uipathcomponent.replace("&auml;", "a")
+        c = uipathcomponent
+        uipathcomponent = uipathcomponent.replace("&aring;", "a")
+        d = uipathcomponent
+        uipathcomponent = uipathcomponent.replace("&ouml;", "o")
+        e = uipathcomponent
+        # remove illigal filepath chars
+        uipathcomponent = re_illigal.sub("_", uipathcomponent)
+        f = uipathcomponent
+        # remove sillyness
+        uipathcomponent = uipathcomponent.replace("__","_").replace("_-_","-")
+        g = uipathcomponent
 
-
-        # if pathcomponent is None:
-        #     print itemtype, children
-        # else:
-        #     print pathcomponent, fspath
         if itemtype == 'GalleryAlbumItem' and children == 1:
-            child_objects.append((uipathcomponent, title,))
+            child_objects.append((uipathcomponent, title, itemtype))
 
             fspath.append(pathcomponent);
 
             uipath.append( uipathcomponent );
-            mkpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test", ('/'.join(uipath))[1:])
-            if not os.path.exists(mkpath) and not dryrun:
+            mkpath = os.path.join(scriptdir, "test", ('/'.join(uipath))[1:])
+            if not os.path.exists(mkpath) and not options['dry_run']:
                 os.makedirs(mkpath)
+                sys.stdout.write('A')
 
             # traverse deeper into the structure if there's childen
             grandchildren = get_children(itemid, fspath, uipath, depth+1);
 
-            # create album html
-            #
             if not grandchildren:
-                print "empty:", ('/'.join(uipath))[1:]
-            elif not dryrun:
-                fname = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test", ('/'.join(uipath))[1:], "index.html")
-                generate_html(fname, grandchildren)
+                sys.stdout.write('a')
+                # print "empty:", ('/'.join(uipath))[1:]
+            elif not options['dry_run']:
+                fname = os.path.join(scriptdir, "test", ('/'.join(uipath))[1:], "index.html")
+                generate_html(fname, grandchildren, itemtype)
 
             fspath.pop()
             uipath.pop()
         elif itemtype == 'GalleryPhotoItem':
-            child_objects.append((pathcomponent, title,))
-            if not dryrun:
-                p = os.path.join(os.path.dirname(os.path.abspath(__file__)), "gall", ('/'.join(fspath))[1:], pathcomponent)
-                t = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test", ('/'.join(uipath))[1:], pathcomponent)
+            child_objects.append((pathcomponent, title, itemtype))
+            if not options['dry_run']:
+                orig_file = os.path.join(scriptdir, "gall", ('/'.join(fspath))[1:], pathcomponent)
+                link_target = os.path.join(scriptdir, "test", ('/'.join(uipath))[1:], uipathcomponent)
+                thumb_target = os.path.join(scriptdir, "test", ('/'.join(uipath))[1:], 't__'+uipathcomponent)
+                album_target = os.path.join(scriptdir, "test", ('/'.join(uipath))[1:], 't__album.jpg')
 
-                e = os.path.isfile(p)
+                try:
+                    e = os.path.isfile(orig_file)
+                except Exception, e:
+                    print ""
+                    print "orig_file"
+                    print e.__class__.__name__, e
+                    print orig_file
+                    sys.exit(1)
+                try:
+                    t = os.path.exists(thumb_target)
+                except Exception, e:
+                    print ""
+                    print e.__class__.__name__, e
+                    print "row[3]", row[3]
+                    print "row[5]", row[5]
+                    print "title", title
+                    print "pathcomponent", pathcomponent
+                    print "thumb_target", thumb_target
+                    print "link_target", link_target
+                    print "orig_file", orig_file
+                    print "uipathcomponent", uipathcomponent
+                    print "--"
+                    print "a", a
+                    print "A", ' '.join(('x'+str(ord(c))+' ' for c in a.replace('\00','')))
+                    print "bool(a)", bool(a.replace('\00',''))
+                    print "b", b
+                    print "c", c
+                    print "d", d
+                    print "e", e
+                    print "f", f
+                    print "g", g
+                    sys.exit(1)
+
                 if not e:
-                    missing_files.append(p)
-                elif not os.path.exists(t):
-                    os.symlink(p, t)
-
-
+                    missing_files.append(orig_file)
+                    sys.stdout.write('-')
+                else:
+                    if (not os.path.exists(thumb_target) and not options['no_thumbs']) or options['force_thumbs']:
+                        try:
+                            im = Image.open(orig_file)
+                            thumb = cropped_thumbnail(im, (150,150))
+                            thumb.save(thumb_target, "JPEG")
+                            if itemid == rows[0][0] and not os.path.exists(album_target):
+                                thumb.save(album_target, "JPEG")
+                            sys.stdout.write('T')
+                        except Exception, e:
+                            sys.stdout.write('Y')
+                    else:
+                        sys.stdout.write('t')
+                    if not os.path.exists(link_target):
+                        try:
+                            os.symlink(orig_file, link_target)
+                            sys.stdout.write('L')
+                        except Exception, e:
+                            print ""
+                            print e.__class__.__name__, e
+                            print (os.path.exists(link_target)), orig_file, "->", link_target
+                            sys.exit(1)
+                    else:
+                        sys.stdout.write('l')
 
             # create image html page
         else:
@@ -139,12 +319,13 @@ def get_children(id, fspath, uipath, depth):
 
 
 try:
-    con = mdb.connect(mysql['hostname'], mysql['username'], mysql['password'], mysql['database']);
+    options = parse_options()
 
+    con = mdb.connect(mysql['hostname'], mysql['username'], mysql['password'], mysql['database']);
     cur = con.cursor()
 
-    fname = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test", "index.html")
-    generate_html(fname, get_children(7, [''], [''], 0));
+    fname = os.path.join(scriptdir, "test", "index.html")
+    generate_html(fname, get_children(7, [''], [''], 0), "GalleryAlbumItem");
 
     print ""
     print "missing", len(missing_files)
