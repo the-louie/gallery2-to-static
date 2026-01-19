e05b41fb42217d41d5e850a8bd7eefef91a58a94
The louie <github@louie.se>
2026-01-19 08:04:15 +0100

    Minor fixes on old TS code

--- index.ts
+++ index.ts
@@ -10,16 +10,26 @@ const sql = sqlUtils(connection, config as Config)
 const main = async (root: number, pathComponent: Array<string> = [], depth: number = 0) => {
     const children = await sql.getChildren(root);
     if (children.length > 0) {
+        const recursivePromises: Promise<void>[] = [];
         children.forEach(child => {
             if (child.hasChildren) {
-                main(child.id, pathComponent.concat([child.pathComponent]), ++depth)
-                --depth
+                recursivePromises.push(main(child.id, pathComponent.concat([child.pathComponent]), depth + 1));
             } else if (child.type === 'GalleryPhotoItem') {
                 child.pathComponent = pathComponent.concat([child.pathComponent]).join('/');
             }
         });
+        await Promise.all(recursivePromises);
         fs.writeFileSync(`./data/${root}.json`, JSON.stringify(children))
     }
 }
 
-main(7)
+(async () => {
+    try {
+        await main(7);
+    } catch (error) {
+        console.error('Error in main:', error);
+        process.exit(1);
+    } finally {
+        connection.end();
+    }
+})();
--- sqlUtils.ts
+++ sqlUtils.ts
@@ -9,41 +9,41 @@ export default (connection: mysql.Connection, config: Config) => {
             ce.${config.gallerySettings.columnPrefix}id as id,    -- main id
             e.${config.gallerySettings.columnPrefix}entityType as type,
             i.${config.gallerySettings.columnPrefix}canContainChildren as hasChildren,
-        
+
             i.${config.gallerySettings.columnPrefix}title as title,
             i.${config.gallerySettings.columnPrefix}description as description,
-        
+
             fse.${config.gallerySettings.columnPrefix}pathComponent as pathComponent,
             i.${config.gallerySettings.columnPrefix}originationTimestamp as timestamp,
-        
+
             pi.${config.gallerySettings.columnPrefix}width as width,
             pi.${config.gallerySettings.columnPrefix}height as height,
-        
+
             di.${config.gallerySettings.columnPrefix}width as thumb_width,
             di.${config.gallerySettings.columnPrefix}height as thumb_height
-        
-        
+
+
         -- relations table
         FROM ${config.gallerySettings.tablePrefix}ChildEntity ce
-        
+
         -- master table
         LEFT JOIN ${config.gallerySettings.tablePrefix}Entity e ON e.${config.gallerySettings.columnPrefix}id=ce.${config.gallerySettings.columnPrefix}id
-        
+
         -- item information
         LEFT JOIN ${config.gallerySettings.tablePrefix}Item i ON i.${config.gallerySettings.columnPrefix}id=ce.${config.gallerySettings.columnPrefix}id
-        
+
         -- file system information
         LEFT JOIN ${config.gallerySettings.tablePrefix}FileSystemEntity fse ON fse.${config.gallerySettings.columnPrefix}id = ce.${config.gallerySettings.columnPrefix}id
-        
+
         -- photo-info
         LEFT JOIN ${config.gallerySettings.tablePrefix}PhotoItem pi ON pi.${config.gallerySettings.columnPrefix}id=ce.${config.gallerySettings.columnPrefix}id
-        
+
         -- thumbinfo
         LEFT JOIN ${config.gallerySettings.tablePrefix}DerivativeImage di on di.${config.gallerySettings.columnPrefix}id = ce.${config.gallerySettings.columnPrefix}id
-        
+
         WHERE
             e.${config.gallerySettings.columnPrefix}entityType in ('GalleryAlbumItem', 'GalleryPhotoItem') AND
-            ${config.gallerySettings.columnPrefix}parentId = ?`
+            ce.${config.gallerySettings.columnPrefix}parentId = ?`
     return {
         getChildren: (itemId: number): Promise<Array<Child>> => new Promise((resolve, reject) => {
             connection.query(SQL_GET_CHILDREN, [itemId], (error, results) => {
